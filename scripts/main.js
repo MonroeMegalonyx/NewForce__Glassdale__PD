7c8735d8a5f97baac67c80bf200ca2d769c33b21
Vincent OLeary <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-01-26 10:38:37 -0500

    Added dark mode button from example

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,14 @@
 import { listCriminals } from './criminals/CriminalList.js';
 
 listCriminals();
+
+const darkModeButton = document.querySelector("#dark-mode");
+
+
+darkModeButton.addEventListener("click", function(){
+    // Select the entire body tag
+    const bodyElement = document.querySelector("body")
+  
+    // Add a class
+    bodyElement.classList.toggle("dark-background")
+  });
