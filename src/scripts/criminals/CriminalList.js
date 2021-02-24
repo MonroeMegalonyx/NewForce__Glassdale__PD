8e99b667643f5f2eb62beac719d649fa8c2edc38
MonroeMegalonyx <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-02-24 11:35:37 -0500

    Added many to many relationships for individuals and facilities

--- src/scripts/criminals/CriminalList.js
+++ src/scripts/criminals/CriminalList.js
@@ -1,40 +1,42 @@
 import { getCriminals, useCriminals } from "./CriminalProvider.js";
 import { stringCriminals } from "./CriminalString.js";
+import { getCriminalFacilities, useCriminalFacilities } from "/scripts/facilities/CriminalFacilityProvider.js"
+import { getFacilities, useFacilities } from "/scripts/facilities/FacilityProvider.js"
 
-//function to list all criminals, called when clicking the criminal tab in menu bar
-export const listCriminals = () => {
-    //    let criminalContainer = document.querySelector(".criminal-list");
-    //    criminalContainer.innerHTML = ``;
+// //function to list all criminals, called when clicking the criminal tab in menu bar
+// export const listCriminals = () => {
+//     //    let criminalContainer = document.querySelector(".criminal-list");
+//     //    criminalContainer.innerHTML = ``;
 
-    // Extra step to first fetch all the data and saving it use getCriminals function, then possible to use it like earlier examples
-    getCriminals().then(() => {
-        let criminals = useCriminals();
-        let CriminalsAsHTMLString = "";
+//     // Extra step to first fetch all the data and saving it use getCriminals function, then possible to use it like earlier examples
+//     getCriminals().then(() => {
+//         let criminals = useCriminals();
+//         let CriminalsAsHTMLString = "";
 
-        //console.table(criminals);
+//         //console.table(criminals);
 
-        for (let i = 0; i < criminals.length; i++) {
-            CriminalsAsHTMLString += stringCriminals(criminals[i]);
-        }
+//         for (let i = 0; i < criminals.length; i++) {
+//             CriminalsAsHTMLString += stringCriminals(criminals[i]);
+//         }
 
-        //console.log(CriminalsAsHTMLString);
+//         //console.log(CriminalsAsHTMLString);
 
-        // get all containers for menu values and set to empty on each click. This renders only the new click content each time.
-        let criminalContainer = document.querySelector(".criminal-list");
-        //criminalContainer.innerHTML = ``;
-        let facilityContainer = document.querySelector(".facility-list");
-        facilityContainer.innerHTML = ``;
-        let officerContainer = document.querySelector(".officer-list");
-        officerContainer.innerHTML = ``;
-        criminalContainer.innerHTML = `${CriminalsAsHTMLString}`;
-        let alibiContainer = document.querySelector(".alibi-list");
-        alibiContainer.innerHTML = ``;
-        let noteContainer = document.querySelector(".note-list");
-        noteContainer.innerHTML = ``;
-        // Hide the notes button
-        document.querySelector(".note-input").innerHTML = "";
-    });
-};
+//         // get all containers for menu values and set to empty on each click. This renders only the new click content each time.
+//         let criminalContainer = document.querySelector(".criminal-list");
+//         //criminalContainer.innerHTML = ``;
+//         let facilityContainer = document.querySelector(".facility-list");
+//         facilityContainer.innerHTML = ``;
+//         let officerContainer = document.querySelector(".officer-list");
+//         officerContainer.innerHTML = ``;
+//         criminalContainer.innerHTML = `${CriminalsAsHTMLString}`;
+//         let alibiContainer = document.querySelector(".alibi-list");
+//         alibiContainer.innerHTML = ``;
+//         let noteContainer = document.querySelector(".note-list");
+//         noteContainer.innerHTML = ``;
+//         // Hide the notes button
+//         document.querySelector(".note-input").innerHTML = "";
+//     });
+// };
 
 document.querySelector("#criminals-nav-link").addEventListener("click", () => {
     listCriminals();
@@ -83,3 +85,75 @@ export const listFilteredCriminals = (convictionFilter, officerFilter) => {
         });
     });
 };
+
+// New code for practice many to many relationships
+export const listCriminals = () => {
+    
+    let criminalContainer = document.querySelector(".criminal-list");
+    criminalContainer.innerHTML = ``;
+    let facilityContainer = document.querySelector(".facility-list");
+    facilityContainer.innerHTML = ``;
+    let officerContainer = document.querySelector(".officer-list");
+    officerContainer.innerHTML = ``;
+    let alibiContainer = document.querySelector(".alibi-list");
+        alibiContainer.innerHTML = ``;
+        
+    // Kick off the fetching of both collections of data
+    getFacilities()
+        .then(getCriminalFacilities)
+        .then(getCriminals)
+        .then(
+            () => {
+                // Pull in the data now that it has been fetched
+                const facilities = useFacilities()
+                const crimFac = useCriminalFacilities()
+                const criminals = useCriminals()
+
+                // Pass all three collections of data to render()
+                render(criminals, facilities, crimFac)
+            }
+        )
+}
+
+const render = (criminalsToRender, allFacilities, allRelationships) => {
+    // Step 1 - Iterate all criminals
+    document.querySelector(".criminal-list").innerHTML = criminalsToRender.map(
+        (criminalObject) => {
+            // Step 2 - Filter all relationships to get only ones for this criminal
+            const facilityRelationshipsForThisCriminal = allRelationships.filter(cf => cf.criminalId === criminalObject.id)
+
+            // Step 3 - Convert the relationships to facilities with map()
+            const facilities = facilityRelationshipsForThisCriminal.map(cf => {
+                const matchingFacilityObject = allFacilities.find(facility => facility.id === cf.facilityId)
+                return matchingFacilityObject
+            })
+
+            // Must pass the matching facilities to the Criminal component
+            return Criminal(criminalObject, facilities)
+        }
+    ).join("")
+}
+
+export const Criminal = (criminalObject, facilities) => {
+    return `
+    <div class="criminal">
+        <h4>${criminalObject.name}</h4>
+        <div class="criminal__details">
+            <p>Convicted for ${criminalObject.conviction}</p>
+            <p>Arrested by ${criminalObject.arrestingOfficer}</p>
+            <p>Incarcerated between:
+                ${new Date(criminalObject.incarceration.start).toLocaleDateString()} and
+                ${new Date(criminalObject.incarceration.end).toLocaleDateString()}
+            </p>
+            <p>Age: ${criminalObject.age}</p>
+            <div>
+                <h4>All Facilities connected to</h4>
+                <ul>
+                    ${facilities.map(f => `<li>${f.facilityName}</li>`).join("")}
+                </ul>
+            </div>
+            <button id="associates--${criminalObject.id}">Show Associates</button>
+        </div>
+    </div>
+    `
+}
--- src/scripts/facilities/FacilityProvider.js
+++ src/scripts/facilities/FacilityProvider.js
@@ -1,19 +1,31 @@
 // Defining the array of criminals locally - empty until we fetch from API
+// let facilities = []
+
+// // Create function to return criminal data once it exists, used in earlier examples
+// export const useFacilities = () => {
+//     return facilities.slice()
+// }
+
+// // Create function to fetch the criminal data from the API and save it as the criminal array locally we are used to working with
+// export const getFacilities = () => {
+//     return fetch("https://criminals.glassdale.us/facilities")
+//         .then(response => response.json())
+//         .then(
+//             parsedFacilities => {
+//                 //console.table(parsedFacilities)
+//                 facilities = parsedFacilities
+//             }
+//         )
+// }
+
 let facilities = []
 
-// Create function to return criminal data once it exists, used in earlier examples
-export const useFacilities = () => {
-    return facilities.slice()
-}
+export const useFacilities = () => facilities.slice()
 
-// Create function to fetch the criminal data from the API and save it as the criminal array locally we are used to working with
 export const getFacilities = () => {
-    return fetch("https://criminals.glassdale.us/facilities")
-        .then(response => response.json())
-        .then(
-            parsedFacilities => {
-                //console.table(parsedFacilities)
-                facilities = parsedFacilities
-            }
-        )
+   return fetch("https://criminals.glassdale.us/facilities")
+    .then(response => response.json())
+    .then(apiData => {
+        facilities = apiData
+    })
 }
